c46ed985ed7dccfdef7f091b62437baecd182470
Temple79 <126321231+Temple79@users.noreply.github.com>
2023-12-06 14:10:07 +0200

    Controllers created

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,38 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", (req, res, next) => {
-  delete req.body._id;
-  const thing = new Book({
-    ...req.body,
-  });
-  Book.save()
-    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
+const booksCTRL = require("../controllers/books")
 
-router.put("/:id", (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-router.delete("/:id", (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-router.get("/:id", (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then((book) => res.status(200).json(book))
-    .catch((error) => res.status(404).json({ error }));
-});
-
-router.get("/", (req, res, next) => {
-  Book.find()
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json({ error }));
-});
+router.post("/", booksCTRL.createBook);
+router.put("/:id", booksCTRL.modifyBook);
+router.delete("/:id", booksCTRL.deleteBook);
+router.get("/:id", booksCTRL.getOneBook);
+router.get("/", booksCTRL.getAllBooks);
 
 module.exports = router;
