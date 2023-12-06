e4d6c43f3d02374e4270c2b3ee73807a1bd2ff13
Temple79 <126321231+Temple79@users.noreply.github.com>
2023-12-06 13:06:36 +0200

    Routes created

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const booksRoutes = require('./routes/books');
 
 const Book = require('./models/book');
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb+srv://Projet_7_OC:Projet7OC@temple.crlt2vg.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -13,26 +24,6 @@ mongoose.connect('mongodb+srv://Projet_7_OC:Projet7OC@temple.crlt2vg.mongodb.net
 
 app.use(express.json());
 
-app.post('/api/books', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Book({
-    ...req.body
-  });
-  book.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/books/:id', (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then(book => res.status(200).json(book))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/books', (req, res, next) => {
-  Book.find()
-    .then(books => res.status(200).json(books))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/books', booksRoutes);
 
 module.exports = app;
