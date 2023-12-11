b2c75a880de527e833f77a086ef96af9b2d96f17
Temple79 <126321231+Temple79@users.noreply.github.com>
2023-12-11 17:29:08 +0200

    Book owner check repaired

--- backend/middleware/isOwner.js
+++ backend/middleware/isOwner.js
@@ -1,8 +1,16 @@
+const Book = require("../models/book");
 
-module.exports = (req, res, next) => { 
-    if (book.userId != req.auth.userId) {
-    res.status(401).json({ message: 'Non-autorisé !' });
-  } else {
-    next();
-  }
-}
+module.exports = (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+      if (book.userId != req.auth.userId) {
+        res.status(401).json({ message: "Not authorized" });
+      } else {
+        next();
+      }
+    })
+    .catch((error) => {
+      console.log("Book -isOwner : ERROR 1 : " + error);
+      res.status(400).json("Une erreur est intervenue lors du check du user");
+    });
+};
